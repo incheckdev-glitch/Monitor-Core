5ede99ff360ee070e2d1c299a26c5587d09a269c
incheckdev-glitch <incheckdev@gmail.com>
2026-09-10 08:31:05 +0300

    Load secure User Activity IP tracker

--- src/ui/index.js
+++ src/ui/index.js
@@ -5,6 +5,7 @@ import { installResponsiveRuntime, ResponsiveRuntime } from './responsiveRuntime
 import './managementCommandCenter.js?v=20260907-mcc1';
 import './systemHealth.js?v=20260907-health1';
 import './userActivity.js?v=20260908-ua1';
+import './userActivityIp.js?v=20260910-uaip1';
 import './resellerManagement.js?v=20260908-rm1';
 import './resellerUserAccess.js?v=20260908-rua1';
 import './resellerViewIsolation.js?v=20260908-rmviewfix1';
@@ -101,5 +102,6 @@ window.InCheck360UI = Object.freeze({
     window.UserAvatarPersistence?.refresh?.();
     window.UserProfileIsolation?.refresh?.();
     window.ResellerViewIsolation?.refresh?.();
+    window.UserActivityIp?.refresh?.();
   }
 });
