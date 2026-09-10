7c35ba7eaf816f7762879bf541f7bec5868897ee
incheckdev-glitch <incheckdev@gmail.com>
2026-09-10 19:06:58 +0300

    Refresh CRM pipeline module cache

--- src/services/notificationDeliveryWorker.js
+++ src/services/notificationDeliveryWorker.js
@@ -1,4 +1,4 @@
-import './darkModeSafetyPatch.js?v=20260910-stability4';
+import './darkModeSafetyPatch.js?v=20260910-salespipeline3';
 import './pwaVercelTestPatch.js?v=20260701-nonadmin-sw-update1';
 import './pwaActiveDevicesPanelPatch.js?v=20260701-active-devices-panel2';
 
