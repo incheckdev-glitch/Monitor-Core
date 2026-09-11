eb23cee3bc42bc82b1bf6b2e724db95af7c01831
incheckdev-glitch <incheckdev@gmail.com>
2026-09-11 14:55:41 +0300

    rollback agreement signer UI patch causing ERP freeze

--- src/services/darkModeSafetyPatch.js
+++ src/services/darkModeSafetyPatch.js
@@ -10,7 +10,6 @@ import './leadIntelligenceBackground.js?v=20260911-li-bg3';
 import './leadIntelligenceContactEnrichment.js?v=20260911-li-contact2';
 import './leadIntelligenceAdmin.js?v=20260910-li-admin1';
 import './proposalApprovalIntegrityGuard.js?v=20260911-proposal-approval-integrity2';
-import './agreementSingleProviderSigner.js?v=20260911-agreement-single-provider-signer1';
 import './crmGridViewStable.js?v=20260910-salespipeline3';
 import './allModuleGridViewStable.js?v=20260906-gridstable1';
 import './statusGridGrouping.js?v=20260904-statusgrid1';
